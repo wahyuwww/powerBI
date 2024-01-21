d1283e4f1feab8a232c97a155d91cadc894d6ae2
wahyuwww <wahyu.aan2508@gmail.com>
2024-01-21 08:21:14 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -3,63 +3,72 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header"> */}
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <PowerBIEmbed
-          embedConfig={{
-            type: "report",
-            id: "c4d820d8-2c6e-4dc8-9c02-18c615b999bd",
-            embedUrl:
-              "https://app.powerbi.com/reportEmbed?reportId=c4d820d8-2c6e-4dc8-9c02-18c615b999bd&autoAuth=true&ctid=e5bed369-6dd4-40d4-9eb4-3d41d776749d",
-            accessToken:
-              "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjVCM25SeHRRN2ppOGVORGMzRnkwNUtmOTdaRSIsImtpZCI6IjVCM25SeHRRN2ppOGVORGMzRnkwNUtmOTdaRSJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvZTViZWQzNjktNmRkNC00MGQ0LTllYjQtM2Q0MWQ3NzY3NDlkLyIsImlhdCI6MTcwNTU4NzQyOCwibmJmIjoxNzA1NTg3NDI4LCJleHAiOjE3MDU1OTI0MzgsImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVFFBeS84VkFBQUF4d09LcGs0OERHS1htY0M1WWZieXVIVFpPMkdMa0pQMnB2bXk0TmQ0ZGpZb05pOTRDbVplM3dIVE5DRnFpOUxyIiwiYW1yIjpbInB3ZCIsInJzYSJdLCJhcHBpZCI6Ijg3MWMwMTBmLTVlNjEtNGZiMS04M2FjLTk4NjEwYTdlOTExMCIsImFwcGlkYWNyIjoiMCIsImRldmljZWlkIjoiY2UxYTA0ZjgtZTJjNC00OGUxLWEwM2YtZTU5NDA5NjQ1YTI3IiwiZmFtaWx5X25hbWUiOiJEd2kgUHVyd2FudG8iLCJnaXZlbl9uYW1lIjoiV2FoeXUiLCJpcGFkZHIiOiIyMDIuODAuMjE4LjUiLCJuYW1lIjoiV2FoeXUgRHdpIFB1cndhbnRvIiwib2lkIjoiNDQ5YjlmNmMtZWM3Yi00NjMzLWJjMTItNDJmMTI0OTA3NmNmIiwicHVpZCI6IjEwMDMyMDAxQUFEQ0ZGNTEiLCJyaCI6IjAuQVQwQWFkTy01ZFJ0MUVDZXREMUIxM1owblFrQUFBQUFBQUFBd0FBQUFBQUFBQUE5QUpFLiIsInNjcCI6InVzZXJfaW1wZXJzb25hdGlvbiIsInNpZ25pbl9zdGF0ZSI6WyJrbXNpIl0sInN1YiI6Ikc3cVNCeW5ha3VxbEk3NkhhVmdtNGk4RHRDeGQzSzBqZXJaUFBwenl1anciLCJ0aWQiOiJlNWJlZDM2OS02ZGQ0LTQwZDQtOWViNC0zZDQxZDc3Njc0OWQiLCJ1bmlxdWVfbmFtZSI6IndhaHl1YWFuQHdlYm1haWwudW1tLmFjLmlkIiwidXBuIjoid2FoeXVhYW5Ad2VibWFpbC51bW0uYWMuaWQiLCJ1dGkiOiJNb1llZjJxdFNFbTZZd2I5bjV0aUFBIiwidmVyIjoiMS4wIiwid2lkcyI6WyJiNzlmYmY0ZC0zZWY5LTQ2ODktODE0My03NmIxOTRlODU1MDkiXX0.khEQpg43iKJMnsAbipDCNH4ipRB_7ac9tYrcvMjuFtObAgRyiirD0wA_lCI1959qsTV9zNKTVSZOoyTSqyZ2Cg_JsmKNTy54n6cFKzInKP5ZE4h1b5t4NN_CiyqRSTJiuJHDuJtyhBI5LOrB-5HsIzIJ-BCa2dUZFXi6tonYlqTVF25g5eFiJcTRYhTfD214Hdx6CviYD1MLwd3BaEtQl5tjm3_dSb4fL_QzIIkLteDEi6-aThbALUhV-LlrAx8xjAPYVKLH8LZCKFEGHN8rLClNutdSVGcDk5d8A0Rs1X1sxI2_vZebTXwMRJ9xYtH3A8MI3pSB6PK9SDGhHReypA",
-            tokenType: models.TokenType.Embed,
-            settings: {
-              panes: {
-                filters: {
-                  visible: false,
-                },
-              },
-            },
-          }}
-          eventHandlers={
-            new Map([
-              [
-                "loaded",
-                function () {
-                  console.log("Report loaded");
-                },
-              ],
-              [
-                "rendered",
-                function () {
-                  console.log("Report rendered");
-                },
-              ],
-              [
-                "error",
-                function (event) {
-                  console.log(event.detail);
-                },
-              ],
-            ])
-          }
-          cssClassName={"Embed-container"}
-          getEmbeddedComponent={(embeddedReport) => {
-            window.report = embeddedReport;
-          }}
-        /> */}
-        <iframe
-          title="Task Power Bi"
-          width="100%"
-          height="686"
-          src="https://app.powerbi.com/reportEmbed?reportId=c4d820d8-2c6e-4dc8-9c02-18c615b999bd&autoAuth=true&ctid=e5bed369-6dd4-40d4-9eb4-3d41d776749d"
-          frameborder="0"
-          allowFullScreen="true"
-        ></iframe>
-      {/* </header> */}
-    </div>
+    <iframe
+      title="Task Power Bi"
+      width="100%"
+      height="686"
+      // style={{marginLeft:-12,marginRight:100}}
+      src="https://app.powerbi.com/reportEmbed?reportId=4ac90339-94ed-4719-b5af-e87f3ede95f7&autoAuth=true&ctid=e5bed369-6dd4-40d4-9eb4-3d41d776749d"
+      frameborder="0"
+      allowFullScreen="true"
+    ></iframe>
+    // <div className="App">
+    //   {/* <header className="App-header"> */}
+    //     {/* <img src={logo} className="App-logo" alt="logo" /> */}
+    //     {/* <PowerBIEmbed
+    //       embedConfig={{
+    //         type: "report",
+    //         id: "c4d820d8-2c6e-4dc8-9c02-18c615b999bd",
+    //         embedUrl:
+    //           "https://app.powerbi.com/reportEmbed?reportId=c4d820d8-2c6e-4dc8-9c02-18c615b999bd&autoAuth=true&ctid=e5bed369-6dd4-40d4-9eb4-3d41d776749d",
+    //         accessToken:
+    //           "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjVCM25SeHRRN2ppOGVORGMzRnkwNUtmOTdaRSIsImtpZCI6IjVCM25SeHRRN2ppOGVORGMzRnkwNUtmOTdaRSJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvZTViZWQzNjktNmRkNC00MGQ0LTllYjQtM2Q0MWQ3NzY3NDlkLyIsImlhdCI6MTcwNTU4NzQyOCwibmJmIjoxNzA1NTg3NDI4LCJleHAiOjE3MDU1OTI0MzgsImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVFFBeS84VkFBQUF4d09LcGs0OERHS1htY0M1WWZieXVIVFpPMkdMa0pQMnB2bXk0TmQ0ZGpZb05pOTRDbVplM3dIVE5DRnFpOUxyIiwiYW1yIjpbInB3ZCIsInJzYSJdLCJhcHBpZCI6Ijg3MWMwMTBmLTVlNjEtNGZiMS04M2FjLTk4NjEwYTdlOTExMCIsImFwcGlkYWNyIjoiMCIsImRldmljZWlkIjoiY2UxYTA0ZjgtZTJjNC00OGUxLWEwM2YtZTU5NDA5NjQ1YTI3IiwiZmFtaWx5X25hbWUiOiJEd2kgUHVyd2FudG8iLCJnaXZlbl9uYW1lIjoiV2FoeXUiLCJpcGFkZHIiOiIyMDIuODAuMjE4LjUiLCJuYW1lIjoiV2FoeXUgRHdpIFB1cndhbnRvIiwib2lkIjoiNDQ5YjlmNmMtZWM3Yi00NjMzLWJjMTItNDJmMTI0OTA3NmNmIiwicHVpZCI6IjEwMDMyMDAxQUFEQ0ZGNTEiLCJyaCI6IjAuQVQwQWFkTy01ZFJ0MUVDZXREMUIxM1owblFrQUFBQUFBQUFBd0FBQUFBQUFBQUE5QUpFLiIsInNjcCI6InVzZXJfaW1wZXJzb25hdGlvbiIsInNpZ25pbl9zdGF0ZSI6WyJrbXNpIl0sInN1YiI6Ikc3cVNCeW5ha3VxbEk3NkhhVmdtNGk4RHRDeGQzSzBqZXJaUFBwenl1anciLCJ0aWQiOiJlNWJlZDM2OS02ZGQ0LTQwZDQtOWViNC0zZDQxZDc3Njc0OWQiLCJ1bmlxdWVfbmFtZSI6IndhaHl1YWFuQHdlYm1haWwudW1tLmFjLmlkIiwidXBuIjoid2FoeXVhYW5Ad2VibWFpbC51bW0uYWMuaWQiLCJ1dGkiOiJNb1llZjJxdFNFbTZZd2I5bjV0aUFBIiwidmVyIjoiMS4wIiwid2lkcyI6WyJiNzlmYmY0ZC0zZWY5LTQ2ODktODE0My03NmIxOTRlODU1MDkiXX0.khEQpg43iKJMnsAbipDCNH4ipRB_7ac9tYrcvMjuFtObAgRyiirD0wA_lCI1959qsTV9zNKTVSZOoyTSqyZ2Cg_JsmKNTy54n6cFKzInKP5ZE4h1b5t4NN_CiyqRSTJiuJHDuJtyhBI5LOrB-5HsIzIJ-BCa2dUZFXi6tonYlqTVF25g5eFiJcTRYhTfD214Hdx6CviYD1MLwd3BaEtQl5tjm3_dSb4fL_QzIIkLteDEi6-aThbALUhV-LlrAx8xjAPYVKLH8LZCKFEGHN8rLClNutdSVGcDk5d8A0Rs1X1sxI2_vZebTXwMRJ9xYtH3A8MI3pSB6PK9SDGhHReypA",
+    //         tokenType: models.TokenType.Embed,
+    //         settings: {
+    //           panes: {
+    //             filters: {
+    //               visible: false,
+    //             },
+    //           },
+    //         },
+    //       }}
+    //       eventHandlers={
+    //         new Map([
+    //           [
+    //             "loaded",
+    //             function () {
+    //               console.log("Report loaded");
+    //             },
+    //           ],
+    //           [
+    //             "rendered",
+    //             function () {
+    //               console.log("Report rendered");
+    //             },
+    //           ],
+    //           [
+    //             "error",
+    //             function (event) {
+    //               console.log(event.detail);
+    //             },
+    //           ],
+    //         ])
+    //       }
+    //       cssClassName={"Embed-container"}
+    //       getEmbeddedComponent={(embeddedReport) => {
+    //         window.report = embeddedReport;
+    //       }}
+    //     /> */}
+    //     <iframe
+    //       title="Task Power Bi"
+    //       width="100%"
+    //       height="686"
+    //       src="https://app.powerbi.com/reportEmbed?reportId=c4d820d8-2c6e-4dc8-9c02-18c615b999bd&autoAuth=true&ctid=e5bed369-6dd4-40d4-9eb4-3d41d776749d"
+    //       frameborder="0"
+    //       allowFullScreen="true"
+    //     ></iframe>
+    //   {/* </header> */}
+    // </div>
   );
 }
 
